257b44ddf548c2583260a0c5f76e7caa1e1f425e
tobiasgrantner <tobias.grantner@gmail.com>
2024-04-23 20:41:20 +0200

    Augmented code with documentation

--- src/chi_squared.py
+++ src/chi_squared.py
@@ -6,11 +6,24 @@ import re
 import json
 
 
-class ChiSquaredJob(MRJob):
+class ChiSquared(MRJob):
+    """
+    This class implements a MapReduce job that calculates the chi-squared values
+    for each token in each category of Amazon reviews and outputs the top k
+    tokens with the highest chi-squared values for each category.
+    """
 
 
     def configure_args(self):
-        super(ChiSquaredJob, self).configure_args()
+        """
+        Configures the command-line arguments for the job.
+
+        Arguments:
+            -k: The number of tokens with top chi-squared values to output for each category.
+            --stopwords: The path to the file containing the stopwords.
+        """
+
+        super(ChiSquared, self).configure_args()
 
         self.FILES = [str(Path(__file__).parent.parent / 'data' / 'stopwords.txt')]
         self.add_file_arg('--stopwords', default='stopwords.txt')
@@ -18,11 +31,35 @@ class ChiSquaredJob(MRJob):
 
 
     def init_stopwords(self):
+        """
+        Initialises the stopwords set from the file specified in the
+        command-line arguments before the preprocessing mapper is called.
+        """
+
         with open(self.options.stopwords, 'r') as file:
             self.stopwords = set(file.read().splitlines())
 
 
     def mapper_preprocessing(self, _, line: str):
+        """
+        Maps the lines of the input file, each containing one review in JSON format,
+        to the corresponding category and a set of tokens contained in the review.
+
+        The review text is tokenised by converting it to lowercase, splitting it
+        and removing short words, stopwords, and duplicates.
+
+        Input:
+            _: The key of the input line. (Ignored)
+            line: The value of the input line, containing a review in JSON format.
+
+        Output:
+            (category, None), 1: For every review, so for every call of the mapper
+                the value 1 is emitted with the token in the key being set to None
+                in order to count the number of reviews in each category.
+            (category, token), 1: For every token in the review, the value 1 is emitted
+                to count the number of reviews in each category containing the token.
+        """
+
         data = json.loads(line)
         category = data['category']
 
@@ -45,15 +82,63 @@ class ChiSquaredJob(MRJob):
 
 
     def combiner_count(self, key: Union[tuple[str, str], None], values: Generator[list[int], None, None]):
+        """
+        Combiner function that sums up the count values for each category and token. combination.
+
+        Input:
+            key: The key of the input values, containing the category and token.
+            values: The values of the input key, containing the count values produced by the mapper.
+
+        Output:
+            key, sum(values): The key of the input values and the sum of the count values. 
+        """
+
         yield key, sum(values)
 
 
     def reducer_count(self, key: Union[tuple[str, str], None], values: Generator[list[int], None, None]):
+        """
+        Reducer function that sums up the count values for each category and token.
+
+        Input:
+            key: The key of the input values, containing the category and token.
+            values: The values of the input key, containing the count values produced by the combiner.
+
+        Output:
+            token, (category, sum(values)): Emits the token as the new key and a
+                tuple containing the category and the count of documents for the
+                category and token combination as the new value.
+        """
+
         category, token = key
         yield token, (category, sum(values))
 
 
     def reducer_token_sum(self, token: str, values: Generator[tuple[str, int], None, None]):
+        """
+        Reducer function that sums up the document counts for each token. Since
+        reviews can only belong to one category, the sum of the document counts
+        over all categories does not contain duplicated counts.
+
+        In the special case of the token being None, the reducer calculates the
+        total number of documents in the dataset.
+
+        Input:
+            token: The token of the input values.
+            values: List containing the category and the document count for the
+                category and token combination.
+
+        Output:
+            category, (token, count, n_t): Emits for each category contained in
+                the values the category as the new key and a tuple containing
+                the token, the count of documents containing the token in the
+                category, and the total number of documents containing the token
+                as the new value.
+                For the special case of the token being None, "count" contains
+                the total number of documents in the category and "n_t" contains
+                the total number of documents in the dataset.
+        """
+
         counts = {category: count for category, count in values}
         n_t = sum(counts.values())
 
@@ -62,8 +147,34 @@ class ChiSquaredJob(MRJob):
 
 
     def reducer_chi_squared(self, category: str, values: Generator[tuple[str, int, int], None, None]):
+        """
+        Reducer function that calculates the chi-squared values for each token 
+        in the category and outputs the top k tokens with the highest chi-squared
+        values.
+
+        Input:
+            category: The category of the input values.
+            values: List containing the token, the count of documents containing
+                the token in the category, and the total number of documents
+                containing the token.
+                For the special case of the token being None, it contains the
+                total number of documents in the category and the total number
+                of documents in the dataset.
+
+        Output:
+            category, [(chi_squared, token)]: Emits the category as the key and a
+                list of tuples containing the chi-squared value and the token as
+                the value. The list is sorted by the chi-squared values in
+                descending order and the tokens in ascending order and contains
+                the top k tokens with the highest chi-squared values.
+        """
+
+        # dictionary of tokens with their counts and total number of documents
         counts = {token: (count, n_t) for token, count, n_t in values}
+        
+        # total number of documents in category and dataset
         n_c, n = counts.pop(None)
+        
         result = []
 
         for token, (a, n_t) in counts.items():
@@ -78,6 +189,10 @@ class ChiSquaredJob(MRJob):
 
 
     def steps(self) -> list[MRStep]:
+        """
+        Defines the steps of the MapReduce job.
+        """
+
         return [
             MRStep(mapper_init=self.init_stopwords,
                    mapper=self.mapper_preprocessing,
@@ -89,4 +204,4 @@ class ChiSquaredJob(MRJob):
 
 
 if __name__ == '__main__':
-    ChiSquaredJob.run()
+    ChiSquared.run()
--- src/runner.py
+++ src/runner.py
@@ -1,14 +1,20 @@
-from chi_squared import ChiSquaredJob
+from chi_squared import ChiSquared
 
+"""
+Runner script for the chi-squared feature selection algorithm.
+Formats the output according to the assignment requirements and collects the
+dictionary of tokens.
+"""
 
 if __name__ == '__main__':
-    job = ChiSquaredJob()
+    job = ChiSquared()
 
     with job.make_runner() as runner:
         runner.run()
         output = sorted(map(tuple, job.parse_output(runner.cat_output())))
         tokens = sorted({token for _, values in output for _, token in values})
 
+        # sorted 
         for key, values in output:
             values: list[list[float, str]]
             value_strings = [f'{value[1]}:{value[0]}' for value in values]
